b3276b68ee87757ce2be69d900ccfcd355bfef86
v4g4x <varun.gawande@gmail.com>
2019-07-18 10:31:22 +0530

    Final Commit Assigment 2

--- Ass2/Part2/Ass2_B.c
+++ Ass2/Part2/Ass2_B.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-void accArr(int arr[], int n)
+void accArr(int arr[], int n)                   //Accepts Array
 {
     int i;
     for (i = 0; i < n; i++)
@@ -13,7 +14,7 @@ void accArr(int arr[], int n)
     }
 }
 
-void printArr(int arr[], int n)
+void printArr(int arr[], int n)                 //Prints Array
 {
     int i;
     for (i = 0; i < n; i++)
@@ -23,7 +24,7 @@ void printArr(int arr[], int n)
     printf("\n");
 }
 
-void bubbleSort(int arr[], int n)
+void bubbleSort(int arr[], int n)               //Sorts using Bubble Sort
 {
     int i, j;
     for (i = 0; i < n - 1; i++)
@@ -40,40 +41,45 @@ void bubbleSort(int arr[], int n)
     }
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
+    int n; 
     printf("This is Parent process\nProcess ID: %d\n", getpid());
-    int n;
     printf("Enter the size of Array: ");
     scanf("%d", &n);
     int *arr = (int *)calloc(sizeof(int), n); //Accept Array
     accArr(arr, n);
     printf("Array Accepted\nIt is as follows: ");
+    printArr(arr, n);                                           //Before Sort
+    printf("Parent Process before Fork Here, it will sort the array\n");
+    bubbleSort(arr, n);
+    printf("Sorted Array: ");                                   //After Sort
     printArr(arr, n);
     int forkID = fork(); //Fork and Create the Child Process
     if (forkID > 0)
     {
-        //Parent Process Here, it will sort the array
-        printf("Parent Process Here, it will sort the array\n");
-        bubbleSort(arr, n);
-        printf("Sorted Array: ");
-        printArr(arr, n);
+        //Parent Process Here, it will wait for child to EXECVE
+        wait(NULL);
+        printf("This is back to a line of parent\n");
     }
     if (forkID == 0)
     {
-        //Child process here, it will wait for Parent to sort the array, it will run EXECVE
-        sleep(1);
+        printf("\n\nThis is Child process\nProcess ID: %d\n", getpid());
         //Convert Integers to Strings
         int i;
-        char argv[n][n];
+        char **argv;
+        argv = (char**)calloc(sizeof(char*),n);
         for (i = 0; i < n; i++)
+        {
+            argv[i] = (char*)calloc(sizeof(char),n);
             sprintf(argv[i], "%d", arr[i]);
-        //Testing if sprintf works
-        for (i = 0; i < n; i++)
-            printf("%s\n", argv[i]);
-        system("gcc Ass2_B2.c -o Ass2_B2");
-        execve("Ass2_B2", argv, NULL);
-    }
+        }
+        argv[n] = NULL;                                         //Final Element must be NULL
+        printf("This is line before EXECVE\n");
+        system("gcc Ass2_B2.c -o Ass2_B2");                     //Compile Second Program during runtime
+        execve("./Ass2_B2", argv, NULL);
 
+        printf("This is a printf after EXECVE\n");              //After EXECVE is done being executed, child process returns to Parent, this line doesn't Run
+    }
     return 0;
 }
